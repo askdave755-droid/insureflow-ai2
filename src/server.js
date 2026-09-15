aaa2a30391a9b505d9a787c0f5664a5b4255b24e
askdave755-droid <askdave755@gmail.com>
2026-09-15 15:57:18 -0400

    Add life-feeder to test boot stability

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const { attachAnnuityRoutes } = require('./routes-annuity');
 
 // Re-enable one at a time to isolate crash
 require('./orchestrator');
-// require('./life-feeder');
+require('./life-feeder');   // automated HasData life/FE lead generation
 // require('./workers/callWorker');
 // require('./lib/sequences');
 
